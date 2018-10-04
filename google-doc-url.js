97dbeff0c24d4855dc5f6145c382df8f0fead26f
Gman0001 <43858278+Gman0001@users.noreply.github.com>
2018-10-04 22:30:50 +0200

    1 line Changed
    Google Sheet connected directly to my Leaflet Map code

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mbGikUp60MzzK557vfU0h3M_nCpSh-8hcrbLN68dTVk/edit#gid=0/';
